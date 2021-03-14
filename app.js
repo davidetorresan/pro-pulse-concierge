046ad8affc8ec867b19fefd32cb210f964e3b8d2
Davide Torresan <davide@davidetorresan.space>
2021-03-14 15:03:11 +0100

    New commit

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ concierge.on('ready', () =>{
 }) 
 
 concierge.on('message', msg => {
+
+    //Clear chat command
+
     if(msg.content.toLocaleLowerCase().startsWith(prefix + 'clear')){
         const clear = async () => {
             let number = 100;
@@ -27,9 +30,12 @@ concierge.on('message', msg => {
         }
         clear()
     }
+
+    //Weather command
+
     if(msg.content.toLocaleLowerCase().startsWith(prefix + 'weather')){
 
-        const args = msg.content.slice(prefix.length).trim().split(' ');
+        const args = msg.content.slice(prefix.length).trim().split(' ')[1];
 
         let data = [];
 
@@ -55,8 +61,20 @@ concierge.on('message', msg => {
                         { name: `🌀 QNH: `, value: `${data.barometer.hpa} hpa (${data.barometer.hg} hg)`, inline: true },
                     )
                     .setFooter('Powered by Pro Pulse Aviation Development Department & CheckWX', 'https://aviation.propulsegaming.com/loghi/logo.png');
-                msg.channel.send(WeatherEmbed)     
+                msg.channel.send(WeatherEmbed)   
+                msg.author.send(args)  
             })
+    }
+
+    //Charts Command
+
+    if(msg.content.toLocaleLowerCase().startsWith(prefix + 'charts')){
+
+        const args = msg.content.slice(prefix.length).trim().split(' ')[1]
+        const link = `https://chartfox.org/${args}`
+
+        msg.author.send(`Hi! \n You can found the charts for ${args} airport here! ${link} \n :warning: You need to log in with a vatsim account to view the cards :warning:`)
+        
     }
 })
 concierge.login(process.env.TOKEN_DISCORD)
